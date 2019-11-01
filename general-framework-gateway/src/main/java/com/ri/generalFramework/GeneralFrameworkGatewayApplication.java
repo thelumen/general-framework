ba7c635c57a3d78933c0e74c97d613636996d915
jiangrijiu <jrj0279@163.com>
2019-11-01 09:38:46 +0800

    新增 spring cloud gateway

---
+++ general-framework-gateway/src/main/java/com/ri/generalFramework/GeneralFrameworkGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ri.generalFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralFrameworkGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneralFrameworkGatewayApplication.class, args);
+    }
+
+}
