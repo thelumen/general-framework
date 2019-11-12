56177bc7b86e917a32bb3d7dd391db3c55ba0c6c
jiangrijiu <jrj0279@163.com>
2019-11-12 14:10:23 +0800

    搭建 认证服务

---
+++ general-framework-sso-service/src/main/java/com/ri/gengeralFramework/GengeralFrameworkSsoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ri.gengeralFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GengeralFrameworkSsoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GengeralFrameworkSsoServiceApplication.class, args);
+    }
+
+}
---
+++ general-framework-sso-service/src/test/java/com/ri/gengeralFramework/GengeralFrameworkSsoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ri.gengeralFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GengeralFrameworkSsoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
